66152b8b17c566103c01d2d69786b45bfb7e47a0
shivani25-coder <shivanidoyal7@gmail.com>
2023-10-15 16:55:54 -0500

    hw3 done

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.get('/bio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'bio.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
